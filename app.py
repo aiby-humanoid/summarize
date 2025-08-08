9b01fb09de15d7d964f7af7a395ddce2d247c6a1
aiby-humanoid <vry.siotuki@gmail.com>
2025-08-08 12:17:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import gradio as gradio
+import gradio as gr
 from transformers import pipeline
 
-get_completion = pipeline("summarization",model = "sshleifer/distilbart-cnn-12-6")
+get_completion = pipeline("summarization",model = "shleifer/distilbart-cnn-12-6")
 
 def summarize(input):
   output = get_completion(input)
@@ -11,4 +11,4 @@ gr.close_all()
 
 demo = gr.Interface(fn=summarize, input="text", output="text")
 
-demo.launch(share=Ture,)
+demo.launch(share=Ture, server_port=8081)
